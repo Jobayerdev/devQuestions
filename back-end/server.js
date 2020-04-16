f66158d964ecc5a69395b1fc0087d4487a0e89e4
Jobayer Hossain <jobayerhossain977@Gmail.com>
2020-04-16 23:07:45 +0600

    init

---
+++ back-end/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+useNewUrlParser: true;
+//MIDDLEWARE
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+//ROUTES
+app.use("/api/category", require("./routers/questionRoutes"));
+
+//DB CONNECTION
+mongoose.connect(
+	process.env.MONGODB_URI,
+	{
+		useFindAndModify: false,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	},
+	() => {
+		console.log("Database connected successfully");
+	}
+);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("../front-end/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../front-end/build", "index.html"));
+	});
+}
+
+app.listen(process.env.PORT || 4000, function () {
+	console.log("Express server listening on port 9000 dev mode");
+});
---
+++ front-end/src/redux/category/category.actions.js
@@ -0,0 +1,71 @@
+import {
+	GET_CATEGORY,
+	GET_SINGLE_CATEGORY,
+	CREATE_QUESTION,
+} from "./category.types";
+import axios from "axios";
+import { alertAction } from "../alert/alertAction";
+
+export const getCategory = () => async (dispatch) => {
+	try {
+		const res = await axios.get(
+			"https://front-end-interview-question.herokuapp.com/api/category"
+		);
+		dispatch({
+			type: GET_CATEGORY,
+			payload: res.data,
+		});
+	} catch (e) {
+		dispatch(alertAction(e.message, "danger"));
+	}
+};
+
+//Get Category by id
+export const getCategoryById = (id) => async (dispatch) => {
+	try {
+		const res = await axios.get(
+			`https://front-end-interview-question.herokuapp.com/api/category/${id.categoryId}`
+		);
+		dispatch({
+			type: GET_SINGLE_CATEGORY,
+			payload: res.data,
+		});
+	} catch (e) {
+		dispatch(alertAction(e.message, "danger"));
+	}
+};
+//CREATE_QUESTION
+export const createQuestion = (id, { questionTitle, answer }) => async (
+	dispatch
+) => {
+	const config = {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+	const body = JSON.stringify({
+		questionTitle,
+		answer,
+	});
+	try {
+		const res = await axios.put(
+			`https://front-end-interview-question.herokuapp.com/api/category/${id}`,
+			body,
+			config
+		);
+
+		dispatch({
+			type: CREATE_QUESTION,
+			payload: res.data,
+		});
+		dispatch(
+			alertAction(
+				"Question Submitted. We will review it soon. Thank You",
+				"Success"
+			)
+		);
+	} catch (e) {
+		console.log(e.message);
+		dispatch(alertAction(e.message, "danger"));
+	}
+};
